ba8f1be4aeba5725b9153d17dd5479cb43e42ad2
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2023-03-05 19:36:17 -0300

    feat: add author field on get all messages

--- src/controllers/message.rs
+++ src/controllers/message.rs
@@ -1,17 +1,20 @@
 use axum::{http::{Request, StatusCode}, Json, response::IntoResponse, Extension};
 use sqlx::{PgPool};
 
-use crate::models::message::{Message, NewMessage};
+use crate::models::message::{Message, NewMessage, MessageWithAuthor};
 
 pub async fn get_all(Extension(pool): Extension<PgPool>) -> impl IntoResponse {
-    let mut result: Result<Vec<Message>, sqlx::Error> = sqlx::query_as::<_, Message>("SELECT * FROM message")
+    let sql = "SELECT m.message_id, u.user_id, u.name AS author, m.text FROM message m INNER JOIN \"user\" u ON m.user_id = u.user_id";
+    let result: Result<Vec<MessageWithAuthor>, sqlx::Error> = 
+        sqlx::query_as::<_, MessageWithAuthor>(sql)
         .fetch_all(&pool)
         .await;
+
     match result {
         Ok(messages) => (StatusCode::OK, Json(messages)),
         Err(err) => {
             println!("error retrieving messages: {}", err.as_database_error().unwrap().to_string());
-            (StatusCode::INTERNAL_SERVER_ERROR, Json(Vec::<Message>::new()))
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(Vec::<MessageWithAuthor>::new()))
         }
     }
 }
@@ -19,7 +22,7 @@ pub async fn get_all(Extension(pool): Extension<PgPool>) -> impl IntoResponse {
 pub async fn create(Extension(pool): Extension<PgPool>, Json(message): Json<NewMessage>) -> impl IntoResponse {
     let sql = "INSERT INTO message (user_id, text) VALUES ($1, $2) RETURNING *";
 
-    let mut result: Result<Message, sqlx::Error> = 
+    let result: Result<Message, sqlx::Error> = 
         sqlx::query_as(sql)
         .bind(&message.user_id)
         .bind(&message.text)
@@ -33,7 +36,7 @@ pub async fn create(Extension(pool): Extension<PgPool>, Json(message): Json<NewM
                 (StatusCode::INTERNAL_SERVER_ERROR, Json(Message {
                     message_id: 0,
                     user_id: 0,
-                    text: String::from("")
+                    text: String::from(""),
                 }))
             }
         }
--- src/models/message.rs
+++ src/models/message.rs
@@ -8,6 +8,14 @@ pub struct Message {
 }
 
 #[derive(sqlx::FromRow, Serialize, Deserialize)]
+pub struct MessageWithAuthor {
+    pub message_id: i32,
+    pub user_id: i32,
+    pub author: String,
+    pub text: String,
+}
+
+#[derive(sqlx::FromRow, Serialize, Deserialize)]
 pub struct NewMessage {
     pub user_id: i32,
     pub text: String,
